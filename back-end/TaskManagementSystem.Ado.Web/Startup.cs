91ab1542e5efabf05f09e9fff910cec5c91bfde8
Mykhailo Trufyn <mishathrufyn@gmail.com>
2023-09-24 18:59:11 +0300

    add startup

---
+++ back-end/TaskManagementSystem.Ado.Web/Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace TaskManagementSystem.Ado.Web
+{
+    public class Startup
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public Startup(IConfigurationRoot configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(app => app.MapControllers());
+        }
+    }
+}
