539d6e7e88c9298e60852cc899c99b08c591b265
Mykhailo Trufyn <mishathrufyn@gmail.com>
2023-09-26 08:14:24 +0300

    remove repositories

--- back-end/TMS.Dapper.DAL/Repositories/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using TaskManagementSystem.Ado.Dall.Entities;
-
-namespace TaskManagementSystem.Ado.Dall.Repositories.Interfaces
-{
-    public interface IUserRepository : IGenericRepository<User>
-    {
-        Task<IEnumerable<User>> GetTopTenUsersAsync();
-    }
-}
